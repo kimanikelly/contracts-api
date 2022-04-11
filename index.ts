5b32a37f35f5204ac7073d7669416782033ad74d
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-04-11 17:52:17 -0400

    Typechain bindings able to be used in the REST api

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
 import { Token__factory } from "@kimanikelly/core-contracts";
+
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get("/tokenContract", (req, res) => {
+  res.json({
+    abi: Token__factory.abi,
+    bytecode: Token__factory.bytecode,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
